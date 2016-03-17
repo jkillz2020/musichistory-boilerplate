68f689daa1d552dd4b684717ea7718ea60770623
Josh Kilgore <joshkilgore@Joshs-MacBook-Pro.local>
2016-03-17 01:02:53 -0500

    add functionality to change page views on link click

--- add.js
+++ add.js
@@ -1,10 +1,11 @@
+"use strict"
 var addLink = document.getElementById("link-add");
 var addView = document.getElementById("add-view");
+var listView = document.getElementById("list-view");
 
 addLink.addEventListener("click", function() {
-  homeView.classList.add("hidden");
   listView.classList.add("hidden");
-
+  listView.classList.remove("visible");
   addView.classList.add("visible");
   addView.classList.remove("hidden");
 
--- list.js
+++ list.js
@@ -1,10 +1,10 @@
+"use strict"
 var listLink = document.getElementById("link-list");
 var listView = document.getElementById("list-view");
+var addView = document.getElementById("add-view");
 
 listLink.addEventListener("click", function(event) {
-  event.preventDefault();
-  homeView.classList.add("hidden");
-  addView.classList.add("hidden");
+addView.classList.add("hidden");
 
   listView.classList.add("visible");
   listView.classList.remove("hidden");
